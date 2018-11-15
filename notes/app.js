276e4a0fe8745633579e250de1f5f3b8ae384467
AndrePurnomo <andre.purnomo@sibasistem.co.id>
2018-11-15 17:45:50 +0700

    Notes App

---
+++ notes/app.js
@@ -0,0 +1,22 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes');
+
+const argv = yargs.argv;
+var command = argv._[0];
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(yargs.title);
+} else if (command === 'remove') {
+  notes.removeNote(yargs.title);
+} else {
+  console.log('Command not recognize');
+}
