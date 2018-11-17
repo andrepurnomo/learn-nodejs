7f7050d202d4d82b07743ebf3e612af4f76dc2f9
Andre Purnomo <andrepurnomo04@gmail.com>
2018-11-17 23:11:47 +0700

    Notes App

--- notes/app.js
+++ notes/app.js
@@ -13,18 +13,26 @@ if (command === "add") {
   var note = notes.addNote(argv.title, argv.body);
   if (note) {
     console.log("Note created");
-    console.log("---");
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    notes.logNote(note);
   } else {
     console.log("Note title taken");
   }
 } else if (command === "list") {
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s)`);
+  allNotes.forEach(note => notes.logNote(note));
 } else if (command === "read") {
-  notes.getNote(yargs.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log("Note created");
+    notes.logNote(note);
+  } else {
+    console.log("Note not found");
+  }
 } else if (command === "remove") {
-  notes.removeNote(yargs.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? "Note was removed" : "Note not found";
+  console.log(message);
 } else {
   console.log("Command not recognize");
 }
--- notes/notes.js
+++ notes/notes.js
@@ -19,15 +19,21 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-  console.log("Getting all notes");
+  return fetchNotes();
 };
 
 var getNote = title => {
-  console.log("Getting note", title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter(note => note.title === title);
+  return filteredNotes[0];
 };
 
 var removeNote = title => {
-  console.log("Remove note", title);
+  var notes = fetchNotes();
+  var new_notes = notes.filter(note => note.title !== title);
+  saveNotes(new_notes);
+
+  return notes.length !== new_notes.length;
 };
 
 var fetchNotes = () => {
@@ -42,9 +48,16 @@ var saveNotes = object => {
   fs.writeFileSync("notes.json", JSON.stringify(object));
 };
 
+var logNote = note => {
+  console.log("---");
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+};
+
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };
